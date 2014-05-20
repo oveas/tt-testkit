3e0e89453c38647d677d220347bdfb83f1b9e2db
Oscar van Eijk <oscar.van.eijk@oveas.com>
2014-05-20 12:41:27 +0200

    Implemented the new installer using XML files

--- install.php
+++ install.php
@@ -27,49 +27,8 @@ define ('TT_ROOT', '/var/www/terra-terra');
 
 require (TT_ROOT . '/TTinstaller.php');
 
-$_id = TTinstaller::installApplication('TTK', 'ttk', 'Terra-Terra TestKit', 'v0.2', 'Testapplication for Terra-Terra', 'https://github.com/oveas/tt-testkit', 'Oscar van Eijk', 'LGPL');
+$_applicationID = TTinstaller::installXMLFile('install.xml');
 
-TTinstaller::addConfig($_id, 'general', 'debug', 16711935);
-TTinstaller::addConfig($_id, 'database', 'prefix', 'ttk_', true);
-TTinstaller::addConfig($_id, 'general', 'js_signal', true);
-
-TTinstaller::addRights($_id
-	,array(
-		 'createtestset'=> 'Can create new testsets'
-		,'createtestcase'=> 'Can create new testcases in an existing testset'
-		,'executetest'=> 'Can execute tests'
-	)
-);
-
-TTinstaller::addGroups($_id
-	,array(
-		 'TTK admin' => 'TT Testkit Administrators'
-		,'TTK Developers' => 'TT developers'
-		,'TTK Testers' => 'TT testers'
-	)
-);
-TTinstaller::addGroupRights($_id
-	,'TTK admin'
-	,array(
-		 'createtestset'
-		,'createtestcase'
-		,'executetest'
-	)
-);
-TTinstaller::addGroupRights($_id
-	,'TTK Developers'
-	,array(
-		 'createtestcase'
-		,'executetest'
-	)
-);
-TTinstaller::addGroupRights($_id
-	,'TTK Testers'
-	,array(
-		 'executetest'
-	)
-);
-
-TTinstaller::enableApplication($_id);
+TTinstaller::enableApplication($_applicationID);
 
 TTloader::getClass('TTrundown.php', TT_ROOT);
